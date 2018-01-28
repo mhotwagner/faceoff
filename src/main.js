ae21eaa098f4f67d0ec3fb7cdf098c74ebb5234a
Michael Hotwagner <mhotwagner@gmail.com>
2018-01-28 08:30:37 -0500

    Add vuex

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import Vue from 'vue';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 
-import App from './App';
-import router from './router';
+import App from '@/App';
+import router from '@/router';
+import store from '@/store';
 
 Vue.config.productionTip = false;
 
@@ -16,6 +17,7 @@ Vue.use(VueMaterial);
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 });
