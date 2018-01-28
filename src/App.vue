a197d00f152d0cebe35c73fa09f14b1371c055e2
Michael Hotwagner <mhotwagner@gmail.com>
2018-01-28 05:55:49 -0500

    Working!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <link rel="stylesheet" href="https://unpkg.com/vue-material@beta/dist/vue-material.min.css">
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-import { MdIcon } from 'vue-material/dist/components';
-
 import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
 
-Vue.use(MdIcon);
+Vue.use(VueMaterial);
 
 /* eslint-disable no-new */
 new Vue({
